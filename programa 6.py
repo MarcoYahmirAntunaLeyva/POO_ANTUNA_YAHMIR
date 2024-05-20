24a4ca025ad791788e03ccf017b807d6236084a3
MarcoYahmirAntunaLeyva <marco_3141230001@utd.edu.mx>
2024-05-21 00:05:07 +0200

    "programa 6 - Mayor Número"

---
+++ programa 6.py
@@ -0,0 +1,11 @@
+numero_1 = int(input("Ingresa el primer numero:"))
+numero_2 = int(input("Ingresa el segundo numero:"))
+
+if numero_1 > numero_2:
+    print("El numero 1 es mayor que el numero 2")
+
+elif numero_1 == numero_2:
+    print("Son iguales")
+    
+else:
+    print("El numero 1 es menor que el numero 2")
