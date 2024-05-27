77fc7f2d94c47b992406e0f0acfef37ff660bdf8
MarcoYahmirAntunaLeyva <marco_3141230001@utd.edu.mx>
2024-05-27 20:28:04 +0200

    "listas"

---
+++ listas.py
@@ -0,0 +1,13 @@
+lista = [1, 2, 3, "Yahmir", 2.5, True]
+lista[3] = "Hola"
+
+lista.append("Mundo")
+lista.insert(0, 0)
+lista.remove(3)
+elemento = lista.pop(4)
+print(elemento)
+print(lista)
+
+#ista_nueva = [1, 2, 3]
+#lista_nueva.extend([4, 5, 6])
+#print(lista_nueva)
